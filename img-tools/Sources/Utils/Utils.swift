544d35a63b1cef5c3eb1de63dcdd6eb36fae4c80
Chris Ridd <chrisridd@mac.com>
2026-03-01 19:55:43 +0000

    eprint now writes to stderr

--- img-tools/Sources/Utils/Utils.swift
+++ img-tools/Sources/Utils/Utils.swift
@@ -2,6 +2,17 @@
 //
 // Copyright Chris Ridd 2026
 
+import Foundation
+
+struct StandardError: TextOutputStream, Sendable {
+    private static let handle = FileHandle.standardError
+
+    public func write(_ string: String) {
+        Self.handle.write(Data(string.utf8))
+    }
+}
+
 public func eprint(_ str : String) {
-    print(str)
+    var stderr = StandardError()
+    print(str, to: &stderr)
 }
